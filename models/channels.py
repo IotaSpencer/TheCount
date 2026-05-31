d6e94a4f90a5b618dfc7cb8f8838410b4f562be3
IotaSpencer <me@iotaspencer.me>
2026-05-31 16:47:14 -0400

    models: convert Channels to declarative mapping and define primary key

--- models/channels.py
+++ models/channels.py
@@ -1,30 +1,20 @@
-from typing import List
 from typing import Optional
-from sqlalchemy import ForeignKey, Integer, Table
+from sqlalchemy import Integer
 from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
 
-from models.base import Base, engine
+from models.base import Base
 
-
-class Channels(Base):
-    __table__ = Table(
-        'channels',
-        Base.metadata,
-        autoload_with=engine
-    )
-    def __repr__(self) -> str:
-        return f"Channels(serverID={self.serverID!r}, channelID={self.channelID}, current_count={self.current_count})"
 
 class Channels(Base):
     __tablename__ = 'channels'
 
     serverID: Mapped[str] = mapped_column(String(64))
     channelID: Mapped[str] = mapped_column(String(64), primary_key=True)
-    current_count: Mapped[int] = mapped_column(Integer(100))
+    current_count: Mapped[int] = mapped_column(Integer)
+    last_userID: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    times_counted: Mapped[int] = mapped_column(Integer, default=0)
 
     def __repr__(self) -> str:
-        return f"Channels(serverID={self.serverID!r}, score={self.score})"
+        return f"Channels(serverID={self.serverID!r}, channelID={self.channelID}, current_count={self.current_count})"
